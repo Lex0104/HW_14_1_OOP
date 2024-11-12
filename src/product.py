8f8399b84bc7c52b904ef003819884f5a341e977
Eduard <barned85@gmail.com>
2024-11-12 15:49:49 +0300

    1) addition src/category - add_method
    2) add tests
    
    3)README.md

--- src/product.py
+++ src/product.py
@@ -4,8 +4,38 @@ class Product:
     price: float
     quantity: int
 
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: object, description: object, price: object, quantity: object) -> object:
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, new_dict_product, products=None):
+        if products:
+            for product in products:
+                if product.name == new_dict_product["name"]:
+                    product.quantity += new_dict_product["quantity"]
+                    product.price = max([product.price, new_dict_product["price"]])
+                    return product
+        return cls(
+            new_dict_product["name"],
+            new_dict_product["description"],
+            new_dict_product["price"],
+            new_dict_product["quantity"],
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, price: float):
+        if price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+            return
+        if price != self.__price:
+            check = input("Изменять цену? Введите y если да,и n если нет.\n")
+            if check != "y":
+                return
+        self.__price = price
--- tests/tests_product.py
+++ tests/tests_product.py
@@ -3,3 +3,16 @@ def test_product_init(first_product):
     assert first_product.description == "512GB, Gray space"
     assert first_product.price == 210000.0
     assert first_product.quantity == 8
+
+
+def test_price_init(system, first_product):
+    first_product.price = 0.0
+    notifications = system.readouterr()
+    assert notifications.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+    first_product.price = -1.0
+    notifications = system.readouterr()
+    assert notifications.out.strip() == "Цена не должна быть нулевая или отрицательная"
+
+    first_product.price = 15000.0
+    assert first_product.price == 15000.0
